7b695b2896ca9b26dd6ebb137255440f6febdabc
edensiter-create <edensiterkol2403@gmail.com>
2026-04-25 20:27:39 +0300

    feat: add user profile update

--- backend/src/controllers/users_controller.ts
+++ backend/src/controllers/users_controller.ts
@@ -26,6 +26,39 @@ class UsersController {
     }
   }
 
+  async updateMe(req: Request, res: Response) {
+  try {
+    const userId = req.params.userId as string;
+
+    const updateData: {
+      userName?: string;
+      profile_picture_uri?: string;
+    } = {};
+
+    if (req.body.userName) {
+      updateData.userName = req.body.userName;
+    }
+
+    if (req.file) {
+      updateData.profile_picture_uri = req.file.filename;
+    }
+
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch {
+    res.status(500).json({ message: "Failed to update user" });
+  }
+}
+
   async create(req: Request, res: Response) {
     try {
       const newUser = await userModel.create(req.body);
--- backend/src/routes/users_routes.ts
+++ backend/src/routes/users_routes.ts
@@ -1,8 +1,22 @@
 import express from "express";
 import { authMiddleware } from "../controllers/auth_controller";
 import usersController from "../controllers/users_controller";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({ storage });
 
 router.get("/", usersController.getAll);
 router.get("/me", authMiddleware, (req, res) =>
@@ -10,5 +24,6 @@ router.get("/me", authMiddleware, (req, res) =>
 );
 router.get("/:id", usersController.getById);
 router.post("/", usersController.create);
+router.put("/me",authMiddleware,upload.single("image"), (req, res) => usersController.updateMe(req, res));
 
 export default router;
