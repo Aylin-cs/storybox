22273bdabfed53827b87f56935119d11ca3eec66
Aylin <aylinhanai12@gmail.com>
2026-04-28 20:19:08 +0300

    Add likes system UI

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,11 +1,27 @@
 import { type Post } from "../services/post-service";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import likeService from "../services/like-service";
+
 interface Props {
   post: Post;
   username: string;
 }
 
 const PostCard = ({ post, username }: Props) => {
+  const [likesCount, setLikesCount] = useState(post.likes?.length || 0);
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = async () => {
+    try {
+      const response = await likeService.toggleLike(post._id).request;
+
+      setLikesCount(response.data.likesCount);
+      setLiked(response.data.liked);
+    } catch (err) {
+      console.error("Failed to like post", err);
+    }
+  };
   return (
     <div style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
       <strong>{username}</strong>
@@ -22,6 +38,11 @@ const PostCard = ({ post, username }: Props) => {
       <Link to={`/posts/${post._id}/comments`}>
         View Comments ({post.comment_count || 0})
       </Link>
+
+      <br />
+      <button onClick={handleLike}>
+        {liked ? "Unlike" : "Like"} ({likesCount})
+      </button>
     </div>
   );
 };
